61c4a6de4fe46978a5f474af6e6bd39d9bc348a2
Abraham Escobedo <abraham94esc@gmail.com>
2025-07-23 15:07:57 -0500

    change copy ssh IP

--- ia_playbook/copy-ssh-key.sh
+++ ia_playbook/copy-ssh-key.sh
@@ -1,1 +1,1 @@
-for i in 5 10 15 20 ; do sshpass -f password.txt ssh-copy-id -o StrictHostKeyChecking=no admin@172.25.64.$i;done
+for i in 5 10 15 20; do sshpass -f password.txt ssh-copy-id -o StrictHostKeyChecking=no admin@192.168.0.0$i; done
